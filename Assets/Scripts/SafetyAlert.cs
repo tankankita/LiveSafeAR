5cefd2fdfd8abee06d7444efef3454b7fa5c031c
Bobby Taylor <bltxr9@gmail.com>
2021-08-27 00:23:25 -0400

    added comment to broken script

--- Assets/Scripts/SafetyAlert.cs
+++ Assets/Scripts/SafetyAlert.cs
@@ -1,68 +1,68 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class SafetyAlert : MonoBehaviour
-{
+// public class SafetyAlert : MonoBehaviour
+// {
 
-    public int floodStartTimer = 20;
+//     public int floodStartTimer = 20;
 
-    public GameObject alertNotifcation;
+//     public GameObject alertNotifcation;
 
-    private bool yes, no;
+//     private bool yes, no;
 
-    private void Start()
-    {
-        Invoke("StartNotification",floodStartTimer);
-    }
+//     private void Start()
+//     {
+//         Invoke("StartNotification",floodStartTimer);
+//     }
     
-    private IEnumerator SafetyCheckupPopup()
+//     private IEnumerator SafetyCheckupPopup()
 
-    {
+//     {
 
-	int maxWait = 10;
+// 	int maxWait = 10;
 
-		while (maxWait > 0)
-	{
-		yield return new WaitForSeconds(1);
-		maxWait--;
-	}
+// 		while (maxWait > 0)
+// 	{
+// 		yield return new WaitForSeconds(1);
+// 		maxWait--;
+// 	}
 
-    if (maxWait <= 0)
-    {
-        alertNotifcation.SetActive(true);
-    } 
+//     if (maxWait <= 0)
+//     {
+//         alertNotifcation.SetActive(true);
+//     } 
 
-    if (yes)
-    {
-        alertNotifcation.SetActive(false);
-        StartCourtine(SafetyCheckupPopup);
-    }
+//     if (yes)
+//     {
+//         alertNotifcation.SetActive(false);
+//         StartCourtine(SafetyCheckupPopup);
+//     }
 
-    // else if (no)
-    // {
-    //     Call911();
-    // }
+//     // else if (no)
+//     // {
+//     //     Call911();
+//     // }
 
-    // }
-    }
+//     // }
+//     }
 
-    private void StartNotification()
-    {
-        StartCourtine(SafetyCheckupPopup)
-    }
+//     private void StartNotification()
+//     {
+//         StartCourtine(SafetyCheckupPopup);
+//     }
 
 
-    public void Yes()
-    {
-        yes = true;
-        no = false;
-    }
+//     public void Yes()
+//     {
+//         yes = true;
+//         no = false;
+//     }
 
-    public void No()
-    {
-        yes = false;
-        no = true;
-    }
+//     public void No()
+//     {
+//         yes = false;
+//         no = true;
+//     }
 
-}
+// }
