db6040e8d398fe5e3997bd48322ef3583c59c788
Youssef15015 <joey070@gmail.com>
2021-08-31 22:29:16 -0500

    Destination Marker Script

---
+++ Assets/Scripts/WarningMapMarker.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private float floodStartTimer = SafetyAlert.GetFloodStartTimer();
+
+    public float _movementTimer = 1;
+
+    private int timer = 0;
+
+    private bool _destinationReached = false;
+
+
+
+
+    void MarkerMoveUp() 
+    {
+      Vector3 startingPos  = transform.position;
+      Vector3 finalPos = transform.position + (transform.up * 0.05f);
+      float elapsedTime = 0;
+
+      while (elapsedTime < _movementTimer) 
+      {
+         transform.position = Vector3.Lerp(startingPos, finalPos, (elapsedTime / _movementTimer));
+         elapsedTime += Time.deltaTime;
+      }
+    }
+
+    void MarkerMoveDown() 
+    {
+      Vector3 startingPos  = transform.position;
+      Vector3 finalPos = transform.position + (-transform.up * 0.05f);
+      float elapsedTime = 0;
+
+      while (elapsedTime < _movementTimer) {
+         transform.position = Vector3.Lerp(startingPos, finalPos, (elapsedTime / _movementTimer));
+         elapsedTime += Time.deltaTime;
+      }
+    }
+
+
+
+    void OnTriggerEnter()
+    {
+     _destinationReached = true;   
+    }
+
+    void Update()
+    {
+        timer +=1;
+        if (timer >= floodStartTimer & _destinationReached)
+        {
+            MarkerMoveUp();
+        }
+
+        if (_movementTimer >= floodStartTimer & _destinationReached)
+        {
+            MarkerMoveDown();
+        }
+
+    }
+}
+
